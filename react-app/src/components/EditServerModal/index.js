75eb97caa3a277dd1838d0adcd5a9033eac9ed5c
Your Name <claudiakosylak@gmail.com>
2023-06-06 09:42:17 -0500

    connected delete server modal to edit server modal

--- react-app/src/components/EditServerModal/index.js
+++ react-app/src/components/EditServerModal/index.js
@@ -3,6 +3,7 @@ import { useModal } from "../../context/Modal";
 import { useDispatch, useSelector } from "react-redux";
 import OpenModalButton from "../OpenModalButton";
 import { getServersThunk, updateServerThunk } from "../../store/server";
+import DeleteServerModal from "../DeleteServerModal";
 
 function EditServerModal({ server }) {
     const user = useSelector(state => state.session.user)
@@ -30,30 +31,33 @@ function EditServerModal({ server }) {
 
     return (
         <>
-        <div className="server-modal-inner-container">
-            <div>
-            {(user.id == server.owner_id) && (
-                
-            )}
-            </div>
-            <div>
-            <h2>Server Overview</h2>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    SERVER NAME
-                    <input
-                        type="text"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        required
-                    ></input>
-                </label>
-                <button type="submit">Save Changes</button>
-            </form>
+            <div className="server-modal-inner-container">
+                <div>
+                    {(user.id == server.owner_id) && (
+                        <OpenModalButton
+                            buttonText="Delete Server"
+                            modalComponent={<DeleteServerModal server={server}/>}
+                        />
+                    )}
+                </div>
+                <div>
+                    <h2>Server Overview</h2>
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            SERVER NAME
+                            <input
+                                type="text"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                required
+                            ></input>
+                        </label>
+                        <button type="submit">Save Changes</button>
+                    </form>
+
+                </div>
 
             </div>
-
-        </div>
         </>
     )
 }
