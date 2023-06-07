a963aa2ccc2c13c220e3ec31ff5229c93215ba52
James Lee <lee963654@gmail.com>
2023-06-07 12:23:49 -0400

    saved changes

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,9 +1,6 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
-<<<<<<< HEAD
 from .server_form import ServerForm
 from .channel_form import ChannelForm
 from .delete_server_form import DeleteServerForm
-=======
 from .update_user_form import UpdateUserForm
->>>>>>> update-user
