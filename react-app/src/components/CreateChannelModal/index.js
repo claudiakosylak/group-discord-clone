e34e38212008d7b8cb35ee6fdc5b2a706fd66e88
James Lee <lee963654@gmail.com>
2023-06-08 18:11:38 -0400

    added validations for create channel

--- react-app/src/components/CreateChannelModal/index.js
+++ react-app/src/components/CreateChannelModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { createChannelThunk, getChannelsThunk } from "../../store/channel";
@@ -8,9 +8,19 @@ function NewChannelModal({serverId}) {
     const [title, setTitle] = useState("");
     const [topic, setTopic] = useState("")
     const [errors, setErrors] = useState("");
+    const [hasErrors, setHasErrors] = useState(false)
     const { closeModal } = useModal();
 
     console.log("THE SERVER ID: ", serverId)
+
+    useEffect(() => {
+        const errors = {}
+
+        if (title.length > 40) errors.title = "Please enter a channel name that is less than 40 characters"
+        if (topic.length > 255) errors.topic = "Please enter a topic that is less than 255 characters"
+
+        setErrors(errors)
+    }, [title, topic, hasErrors])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -20,21 +30,24 @@ function NewChannelModal({serverId}) {
             topic
         }
 
-        const response = await dispatch(createChannelThunk(channelInfo, serverId))
-        console.log("THE RESPONSE: ", response)
-        if (response.errors) {
-            setErrors(response.errors)
-            return errors
+        if (Object.values(errors).length) {
+            setHasErrors(true)
         } else {
+            const response = await dispatch(createChannelThunk(channelInfo, serverId))
             await dispatch(getChannelsThunk(serverId))
+            setHasErrors(false)
             closeModal()
+
         }
+
     }
 
     return (
         <>
             <h2>Create Channel</h2>
             <form onSubmit={handleSubmit}>
+            {errors.title ? <p style={{color:"darkred"}}>{errors.title}</p> : ""}
+            {errors.topic ? <p style={{color:"darkred"}}>{errors.topic}</p> : ""}
                 <label>
                     CHANNEL NAME
                     <input
