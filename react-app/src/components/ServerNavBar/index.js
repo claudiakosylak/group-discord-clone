04a0933cfc77b11e7bffffbeb2dde46c6aa1f14b
Your Name <claudiakosylak@gmail.com>
2023-06-09 16:54:21 -0500

    basic home page together and fixed stretching link on signup page

--- react-app/src/components/ServerNavBar/index.js
+++ react-app/src/components/ServerNavBar/index.js
@@ -6,19 +6,22 @@ import "./ServerNavBar.css"
 import { logout } from "../../store/session";
 import ServersList from '../ServersList';
 import Topbar from '../TopBar';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 function ServerNavBar({ isLoaded }) {
   const user = useSelector(state => state.session.user)
   const servers = useSelector(state => state.server.allServers)
   const serversArray = Object.values(servers)
+  console.log("🍎servers: ", servers)
+  console.log("🍎serversArray at zero: ", serversArray[0])
 
   const history = useHistory()
   const dispatch = useDispatch()
 
 
   if (!user) {
-    history.push('/login')
+    return <Redirect to="/login" />
   }
 
   const handleLogout = (e) => {
@@ -36,37 +39,39 @@ function ServerNavBar({ isLoaded }) {
         </div>
       )}
 
-      {user && (
-        <div className="left-nav-bars-container">
-          <div className="top-bar-wrapper">
-            <Topbar />
+
+      <div className="left-nav-bars-container">
+        <div className="top-bar-wrapper">
+          <Topbar />
+        </div>
+        <div className="left-nav-bars">
+
+          <div className="user-info-wrapper">
+
+            <img src={user.profile_pic} className="user-profile-pic"></img>
+            <p>{user.username}</p>
+            <p>User Settings</p>
+            <button onClick={handleLogout}>Logout</button>
           </div>
-          <div className="left-nav-bars">
 
-            {user && (
-              <div className="user-info-wrapper">
+          <div className="home-main-content-wrapper">
+            <img className="home-page-image" src="https://mcdn.wallpapersafari.com/medium/48/64/VQ9gG1.jpg"></img>
 
-                <img src={user.profile_pic} className="user-profile-pic"></img>
-                <p>{user.username}</p>
-                <p>User Settings</p>
-                <button onClick={handleLogout}>Logout</button>
+            {( serversArray[0] && serversArray[0].id) ? (
+              <div className="home-main-content-text">
+                <h2>Welcome back! Please select a server to get started.</h2>
+              </div>
+            ) : (
+              <div className="home-main-content-text">
+              <h2>Welcome! You don't seem to be a member of any server yet. Check out our discover page to find your community! </h2>
               </div>
             )}
-            <div className="home-main-content-wrapper">
-              <img className="home-page-image" src="https://cdna.artstation.com/p/assets/images/images/004/187/762/large/alena-aenami-7p-m-1k.jpg?1481159743"></img>
-
-              {/* {serversArray[0] ? (
-              <div>
-
-              </div>
-
-              )} */}
-            </div>
-
           </div>
 
         </div>
-      )}
+
+      </div>
+
     </div >
   )
 }
