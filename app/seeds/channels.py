4950422be1c59a8a68f60359a93cb874fd4cb79d
Your Name <claudiakosylak@gmail.com>
2023-06-28 19:10:26 -0500

    added channel seeders

--- app/seeds/channels.py
+++ app/seeds/channels.py
@@ -2,7 +2,32 @@ from app.models import db, Channel, environment, SCHEMA
 from sqlalchemy.sql import text
 from datetime import datetime
 
+channel_topics = [
+    "General Chat", "Announcements", "Introductions", "Off-Topic Discussions", "News and Current Events",
+    "Q&A and Help", "Gaming Corner", "Music Recommendations", "Art and Creativity", "Memes and Funny Content",
+    "Tech Talk", "Coding and Development", "Book Club", "Movie Reviews", "TV Series Discussions",
+    "Anime and Manga", "Sports Talk", "Fitness and Wellness", "Travel Stories", "Food and Recipes",
+    "Fashion and Style", "Photography Showcase", "Pet Lovers", "Science and Technology", "Debates and Discussions",
+    "Language Exchange", "Writing and Poetry", "Career Advice", "Education and Learning", "Gaming News and Updates",
+    "Virtual Events and Meetups", "Design and Graphics", "Podcast Recommendations", "Mental Health Support",
+    "LGBTQ+ Community", "Relationship Advice", "Finance and Investing", "DIY Projects", "Gardening Tips",
+    "Coding Challenges", "Productivity Hacks", "Fitness Challenges and Goals", "Movie Trivia", "Fan Theories",
+    "Art Collaborations", "Coding Help and Troubleshooting", "Game Tutorials and Tips", "Fashion Trends and Tips",
+    "Virtual Reality Discussions", "Server Feedback and Suggestions"
+]
+
+
 def seed_channels():
+    channel_topic = 0
+
+    for x in range(1, 10):
+        for y in range(1, 5):
+            channel = Channel(
+                title=channel_topics[channel_topic], server_id = x, topic=channel_topics[channel_topic]
+            )
+            channel_topic += 1
+            db.session.add(channel)
+    db.session.commit()
 
     channel1 = Channel(
         title='general', server_id = 1, topic='This is the channel 1 test topic'
--- app/seeds/memberships.py
+++ app/seeds/memberships.py
@@ -3,24 +3,39 @@ from sqlalchemy.sql import text
 from datetime import datetime
 
 def seed_memberships():
-    mem1 = Membership(
-        role = 'member', server_id=1, user_id=2
-    )
-    mem2 = Membership(
-        role = 'member', server_id=2, user_id=1
-    )
-    mem3 = Membership(
-        role = 'member', server_id=2, user_id=3
-    )
-    mem4 = Membership(
-            role = 'member', server_id=1, user_id=3
-        )
-
-    db.session.add(mem1)
-    db.session.add(mem2)
-    db.session.add(mem3)
-    db.session.add(mem4)
+    for x in range(1, 10):
+        for y in range(1, 4):
+            membership = Membership(
+                role = 'member', server_id = x, user_id = x * y
+            )
+            db.session.add(membership)
     db.session.commit()
+
+    for x in range(2, 10):
+        membership2 = Membership(
+            role = 'member', server_id = x, user_id = 1
+        )
+        db.session.add(membership2)
+    db.session.commit()
+
+    # mem1 = Membership(
+    #     role = 'member', server_id=1, user_id=2
+    # )
+    # mem2 = Membership(
+    #     role = 'member', server_id=2, user_id=1
+    # )
+    # mem3 = Membership(
+    #     role = 'member', server_id=2, user_id=3
+    # )
+    # mem4 = Membership(
+    #         role = 'member', server_id=1, user_id=3
+    #     )
+
+    # db.session.add(mem1)
+    # db.session.add(mem2)
+    # db.session.add(mem3)
+    # db.session.add(mem4)
+    # db.session.commit()
 
 
 def undo_memberships():
