ff9bea77674335ab19c4fd6f5ee4ed155544e1e9
Your Name <claudiakosylak@gmail.com>
2023-07-12 16:27:36 -0500

    added seeders for server 3

--- app/seeds/channel_messages.py
+++ app/seeds/channel_messages.py
@@ -35,6 +35,20 @@ conversation = [
     [9, "Wishing everyone a productive meetup. Sunday, here we come!"]
 ]
 
+conversation2 = [
+    [4, "Hey everyone, did you see the news about that car crash? It looked pretty severe."],
+    [6, "Yes, I saw it! It was a terrifying scene. I hope everyone involved is okay."],
+    [8, "Oh no, that's terrible! Accidents like these are always so tragic. I hope they get the help they need."],
+    [10, "I missed the news. Can someone fill me in on what happened?"],
+    [12, "There was a multi-vehicle collision on the highway. It caused a major traffic jam and emergency services are on the scene."],
+    [4, "I saw a few clips online. The wreckage was extensive. I hope they can determine the cause and prevent similar incidents in the future."],
+    [6, "Absolutely! It's a reminder of how important it is to drive safely and be aware of our surroundings on the road."],
+    [8, "It's crucial to follow traffic rules and maintain a responsible attitude while driving. Safety should always be our top priority."],
+    [10, "Thanks for the update, Sophia. It's a sobering reminder to be cautious and mindful while on the road."],
+    [12, "You're welcome, Isabella. Let's hope everyone involved in the accident recovers quickly and that we can learn from this incident."],
+]
+
+
 introductions = [
     [1, "Hey everyone! I'm Demo, and I'm excited to be here. I'm a beginner software developer with a passion for web development."],
     [2, "Hello, fellow developers! I'm Marnie. I'm new to coding, but I'm eager to learn and contribute to our project."],
@@ -47,6 +61,62 @@ introductions = [
     [9, "Greetings, team members! I'm Aiden, an aspiring software developer. Excited to embark on this project and grow together."],
     [10, "Hey everyone! I'm Isabella, a novice programmer eager to dive into this project. Let's make our mark in the world of software development!"]
 ]
+
+off_topic_convo = [
+    [4, "Hey everyone! Have you ever seen a walrus? They are so cute!"],
+    [6, "Yes, I completely agree! Walruses have this adorable charm about them."],
+    [8, "Absolutely! Those big tusks and their plump bodies make them irresistibly cute."],
+    [10, "I've never seen a walrus in person, but they always look adorable in pictures and videos."],
+    [12, "Walruses are incredible creatures. Their whiskers and blubbery appearance just make them even more endearing."],
+    [4, "Did you know that walruses are highly social animals? They form tight-knit communities in the wild."],
+    [6, "That's fascinating! It's heartwarming to think about how walruses interact and care for one another."],
+    [8, "Absolutely! It's incredible to see how they communicate and show affection within their groups."],
+    [10, "I'd love to see a walrus in person someday. I can only imagine how adorable they must be up close."],
+    [12, "If you ever get the chance, don't miss it! They have such a unique and lovable presence. You'll be smitten."],
+]
+
+local_election = [
+    [4, "Hey everyone! Just a friendly reminder to make sure you're registered to vote in your local elections."],
+    [6, "That's a great point! Local elections have a direct impact on our communities. Every vote counts!"],
+    [8, "Absolutely! It's crucial to participate and have a say in the decisions that shape our neighborhoods."],
+    [10, "I completely agree. Local elections often decide policies that directly affect our schools, parks, and infrastructure."],
+    [12, "Remembering to vote is an essential civic duty. Let's encourage everyone we know to get out there and make their voices heard!"],
+]
+
+anime = [
+    [6, "Hey everyone! I can't contain my excitement about the latest anime series. It's absolutely amazing!"],
+    [9, "Oh, I'm glad you mentioned it! I've been looking for a new anime to watch. Which series are you talking about?"],
+    [12, "I think I know the one! The animation and storyline are top-notch. I've been hooked since the first episode."],
+    [15, "You guys are talking about that new fantasy anime, right? I've heard nothing but great things about it!"],
+    [18, "I'm always up for some good anime recommendations. What's the name of this series, and where can I watch it?"],
+    [6, "The anime is called 'Chronicles of the Elements.' You can watch it on a popular streaming platform. Trust me, it's worth it!"],
+    [9, "Thanks for sharing, Emma! I'll definitely add it to my watchlist. I'm excited to dive into this captivating series."],
+    [12, "Logan, you won't be disappointed. The animation quality and character development are outstanding."],
+    [15, "That's what I've been hearing! I can't wait to start watching it. It sounds like a true masterpiece."],
+    [18, "Thanks for the recommendation, Emma. I'll make sure to check it out. I'm always in search of compelling anime to enjoy."],
+]
+
+qa = [
+    [6, "Hey, does anyone know how to add a new channel to the server? I think we could benefit from having a dedicated channel for game discussions."],
+    [9, "That's a great idea! However, only the server owner has the permission to create new channels. We should reach out to them and suggest it."],
+    [12, "Exactly, Emma. It's the server owner's responsibility to manage the channels and permissions. They can create a new channel for us if they find it suitable."],
+    [15, "I agree. It's best to ask the server owner about adding a new channel. They can assess if it aligns with the server's purpose and make the necessary arrangements."],
+    [18, "Thanks for clarifying, Aiden and Sophia. I'll message the server owner and propose the idea. Hopefully, they will consider adding a game discussions channel."],
+]
+
+gaming = [
+    [6, "Hey everyone! What games have you been playing lately? I'm looking for some recommendations."],
+    [9, "I've been hooked on a new RPG game. The storytelling and immersive world are incredible. You should definitely give it a try!"],
+    [12, "I've been exploring the world of competitive online gaming. It's intense but so much fun!"],
+    [15, "I've been diving into a multiplayer shooter. The fast-paced action and teamwork make it a thrilling experience."],
+    [18, "I've been enjoying a strategy game that challenges my critical thinking skills. It's a great way to unwind after a long day."],
+    [6, "Thanks for the suggestions! I'll definitely check them out. Gaming is such a fantastic way to relax and have fun."],
+    [9, "Absolutely! Gaming allows us to escape into different worlds and experiences. It's a great form of entertainment."],
+    [12, "Gaming also helps me connect with friends from around the world. It's amazing how games can bring people together."],
+    [15, "I agree, Sophia! The online gaming community is so diverse and vibrant. It's like a whole new social network."],
+    [18, "Gaming has evolved so much over the years. It's impressive how it can be both a form of entertainment and a platform for socializing."],
+]
+
 
 
 
@@ -64,6 +134,27 @@ def seed_channel_messages():
         db.session.add(new_message)
     db.session.commit()
 
+    for message in conversation2:
+        new_message = ChannelMessage(
+        content = message[1], user_id = message[0], channel_id = 4
+        )
+        db.session.add(new_message)
+    db.session.commit()
+
+    for message in off_topic_convo:
+        new_message = ChannelMessage(
+        content = message[1], user_id = message[0], channel_id = 5
+        )
+        db.session.add(new_message)
+    db.session.commit()
+
+    for message in local_election:
+        new_message = ChannelMessage(
+        content = message[1], user_id = message[0], channel_id = 6
+        )
+        db.session.add(new_message)
+    db.session.commit()
+
     for message in announcement_messages:
         new_message = ChannelMessage(
             content = message, user_id = 1, channel_id = 2
@@ -74,6 +165,27 @@ def seed_channel_messages():
     for message in introductions:
         new_message = ChannelMessage(
             content=message[1], user_id = message[0], channel_id = 3
+        )
+        db.session.add(new_message)
+    db.session.commit()
+
+    for message in anime:
+        new_message = ChannelMessage(
+            content=message[1], user_id = message[0], channel_id = 7
+        )
+        db.session.add(new_message)
+    db.session.commit()
+
+    for message in qa:
+        new_message = ChannelMessage(
+            content=message[1], user_id = message[0], channel_id = 8
+        )
+        db.session.add(new_message)
+    db.session.commit()
+
+    for message in gaming:
+        new_message = ChannelMessage(
+            content=message[1], user_id = message[0], channel_id = 9
         )
         db.session.add(new_message)
     db.session.commit()
